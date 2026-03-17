1ce46a7614e86899092723bfbfc9341f470410a9
Yatsenko Dmytro <sonituensem@gmail.com>
2026-03-17 12:29:42 +0200

    Solution

--- db/models.py
+++ db/models.py
@@ -14,3 +14,56 @@ class Actor(models.Model):
 
     def __str__(self) -> str:
         return f"{self.first_name} {self.last_name}"
+
+
+class Movie(models.Model):
+    title: models.CharField = models.CharField(
+        max_length=255,
+    )
+    description: models.TextField = models.TextField()
+
+    actors: models.ManyToManyField = models.ManyToManyField(
+        "Actor",
+        related_name="movies",
+    )
+    genres: models.ManyToManyField = models.ManyToManyField(
+        "Genre",
+        related_name="movies",
+    )
+
+    def __str__(self) -> str:
+        return self.title
+
+
+class CinemaHall(models.Model):
+    name: models.CharField = models.CharField(
+        max_length=255,
+    )
+    rows: models.IntegerField = models.IntegerField()
+    seats_in_row: models.IntegerField = models.IntegerField()
+
+    def __str__(self) -> str:
+        return self.name
+
+    @property
+    def capacity(self) -> int:
+        return self.rows * self.seats_in_row
+
+
+class MovieSession(models.Model):
+    show_time: models.DateTimeField = models.DateTimeField()
+
+    cinema_hall: models.ForeignKey = models.ForeignKey(
+        "CinemaHall",
+        on_delete=models.CASCADE,
+        related_name="sessions",
+    )
+
+    movie: models.ForeignKey = models.ForeignKey(
+        "Movie",
+        on_delete=models.CASCADE,
+        related_name="sessions",
+    )
+
+    def __str__(self) -> str:
+        return f"{self.movie.title} {self.show_time}"
---
+++ services/movie_session.py
@@ -0,0 +1,60 @@
+from datetime import datetime
+from typing import Optional
+
+from django.db import transaction
+
+from db.models import MovieSession
+from django.db import models
+
+
+def create_movie_session(
+    movie_show_time: datetime,
+    movie_id: int,
+    cinema_hall_id: int,
+) -> MovieSession:
+    return MovieSession.objects.create(
+        show_time=movie_show_time,
+        movie_id=movie_id,
+        cinema_hall_id=cinema_hall_id,
+    )
+
+
+def get_movies_sessions(
+    session_date: Optional[str] = None,
+) -> "models.QuerySet[MovieSession]":
+    queryset = MovieSession.objects.all()
+
+    if session_date:
+        queryset = queryset.filter(show_time__date=session_date)
+
+    return queryset
+
+
+def get_movie_session_by_id(movie_session_id: int) -> MovieSession:
+    return MovieSession.objects.get(id=movie_session_id)
+
+
+@transaction.atomic
+def update_movie_session(
+    session_id: int,
+    show_time: Optional[datetime] = None,
+    movie_id: Optional[int] = None,
+    cinema_hall_id: Optional[int] = None,
+) -> MovieSession:
+    session = MovieSession.objects.get(id=session_id)
+
+    if show_time is not None:
+        session.show_time = show_time
+
+    if movie_id is not None:
+        session.movie_id = movie_id
+
+    if cinema_hall_id is not None:
+        session.cinema_hall_id = cinema_hall_id
+
+    session.save()
+    return session
+
+
+def delete_movie_session_by_id(session_id: int) -> None:
+    MovieSession.objects.filter(id=session_id).delete()
